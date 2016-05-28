0ce19ad8e45e3c79ed4c578b4269be485ed76086
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-05-28 18:42:32 +0300

    no message

--- registration.php
+++ registration.php
@@ -1,3 +1,3 @@
 <?php
 use Magento\Framework\Component\ComponentRegistrar;
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Dfe_TwoCheckout', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Dfe_AmazonPayments', __DIR__);

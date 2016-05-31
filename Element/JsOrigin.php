e5f500dc9be5b2ce33c4a07e5a50b014a4434856
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-05-31 13:26:16 +0300

    no message

---
+++ Element/JsOrigin.php
@@ -0,0 +1,35 @@
+<?php
+namespace Dfe\AmazonPayments\Element;
+// 2016-05-30
+class JsOrigin extends \Df\Framework\Data\Form\Element\Url {
+	/**
+	 * 2016-05-31
+	 * @override
+	 * @see \Df\Framework\Data\Form\Element\Url::messageForThirdPartyLocalhost()
+	 * @used-by \Df\Framework\Data\Form\Element\Url::getElementHtml()
+	 * @return string
+	 */
+	protected function messageForThirdPartyLocalhost() {return $this->messageForOthers();}
+
+	/**
+	 * 2016-05-31
+	 * @override
+	 * @see \Df\Framework\Data\Form\Element\Url::url()
+	 * @return string
+	 */
+	protected function url() {
+		if (!isset($this->{__METHOD__})) {
+			$this->{__METHOD__} = df_url_strip_path(parent::url());
+		}
+		return $this->{__METHOD__};
+	}
+
+	/**
+	 * 2016-05-31
+	 * @override
+	 * @see \Df\Framework\Data\Form\Element\Url::urlForMyLocalPc()
+	 * @used-by \Df\Framework\Data\Form\Element\Url::url()
+	 * @return string
+	 */
+	protected function urlForMyLocalPc() {return $this->urlForOthers();}
+}

1cb8b56bd17630ee18ff8fcd15662d265052b314
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-13 03:50:25 +0300

    no message

--- Element/JsOrigin.php
+++ Element/JsOrigin.php
@@ -1,6 +1,11 @@
 <?php
 namespace Df\Amazon\Element;
-// 2016-05-30
+/**
+ * 2016-05-30
+ * @final We are unable to specify it explicitly with the «final» PHP keyword
+ * because Magento 2 will autogenerate a subclass:
+ * https://github.com/magento/devdocs/blob/713b78a/guides/v2.0/extension-dev-guide/code-generation.md#when-is-code-generated
+ */
 class JsOrigin extends \Df\Framework\Form\Element\Url {
 	/**
 	 * 2016-05-31
@@ -17,7 +22,7 @@ class JsOrigin extends \Df\Framework\Form\Element\Url {
 	 * @see \Df\Framework\Form\Element\Url::url()
 	 * @return string
 	 */
-	protected function url() {return dfc($this, function() {return df_url_strip_path(parent::url());});}
+	protected function url() {return dfc($this, function() {return df_url_base(parent::url());});}
 
 	/**
 	 * 2016-05-31

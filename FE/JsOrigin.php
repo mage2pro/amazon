9a0a325b69be1992ab3459161c69bd5bea1ddc7d
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-12 06:23:40 +0300

    1.0.15

--- FE/JsOrigin.php
+++ FE/JsOrigin.php
@@ -1,17 +1,8 @@
 <?php
-namespace Df\Amazon\Element;
+namespace Df\Amazon\FE;
 // 2016-05-30
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
 class JsOrigin extends \Df\Framework\Form\Element\Url {
-	/**
-	 * 2016-05-31
-	 * @override
-	 * @see \Df\Framework\Form\Element\Url::messageForThirdPartyLocalhost()
-	 * @used-by \Df\Framework\Form\Element\Url::getElementHtml()
-	 * @return string
-	 */
-	final protected function messageForThirdPartyLocalhost() {return $this->messageForOthers();}
-
 	/**
 	 * 2016-05-31
 	 * @override
